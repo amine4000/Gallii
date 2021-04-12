8941ca5bc353f2c014681e96f1456aee82665dac
Amine <toptech6@gmail.com>
2021-04-12 08:04:42 +0100

    mapped through the snaps and added a unique id

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -6,7 +6,10 @@ const useFirestore=(collection)=>{
 
     useEffect(()=>{
         const unsub=projectFirestore.orderBy('createdAt', 'desc').onSnapshot((snap)=>{
-           
+            let documents=[]
+            snap.forEach(doc=>{
+                documents.push({...doc.data(), id:doc.id})
+            })
             setDocs(documents)
             return ()=>unsub()
         })
